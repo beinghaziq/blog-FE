c4c591997e5f32ece635ca5d664c24e0c50066d8
Muhammad Haziq <beinghaziq@gmail.com>
2024-01-17 22:25:58 +0500

    hash created for blog paths

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -1,21 +1,27 @@
 import { baseService } from "./baseService";
 
+export const blogActions = {
+  allBlogs: "/blog",
+  createBlog: "/blogs",
+  singleBlog: (blog_id) => `/blog/${blog_id}`,
+};
+
 export function getBlogs() {
-  return baseService.get("/blog");
+  return baseService.get(blogActions.allBlogs);
 }
 
 export function getBlog(blog_id) {
-  return baseService.get(`/blogs/${blog_id}`);
+  return baseService.get(blogActions.singleBlog(blog_id));
 }
 
 export function createBlog(body) {
-  return baseService.post('/blogs', body);
+  return baseService.post(blogActions.createBlog, body);
 }
 
 export function deleteBlog(blog_id) {
-  return baseService.delete(`/blogs/${blog_id}`);
+  return baseService.delete(blogActions.singleBlog(blog_id));
 }
 
 export function updateBlog(blog_id, body) {
-  return baseService.put(`/blogs/${blog_id}`, body);
+  return baseService.put(blogActions.singleBlog(blog_id), body);
 }
